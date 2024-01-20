49875912b08a9e7d74ca63e074ba0f8463b5dd2a
svshatunova <svshatun0va@yandex.ru>
2024-01-20 12:03:36 +0300

    yacut

--- yacut/views.py
+++ yacut/views.py
@@ -2,7 +2,7 @@ import random
 import string
 from urllib.parse import urljoin
 
-from flask import flash, redirect, render_template # Markup
+from flask import flash, redirect, render_template, Markup
 
 
 from . import app, db, BASE_URL, SHORT_LENGTH
@@ -33,9 +33,9 @@ def index_view():
         db.session.add(new_url)
         db.session.commit()
         short_link = urljoin(BASE_URL, new_url.short)
-        # flash(Markup
-              # (f'Ваша новая ссылка готова: <a href="'
-               # f'{short_link}">{short_link}</a>'))
+        flash(Markup
+              (f'Ваша новая ссылка готова: <a href="'
+               f'{short_link}">{short_link}</a>'))
 
     return render_template('index.html', form=form)
 
